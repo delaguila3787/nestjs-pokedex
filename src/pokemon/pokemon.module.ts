31f52ec1b22b2893eb51af9def894b387fedd466
mauricio del aguila <delaguila3787@gmail.com>
2025-06-07 01:17:44 -0300

    tarea Antes de insertar por lote

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -14,6 +14,7 @@ import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
         schema: PokemonSchema,
       }
     ])
-  ]
+  ],
+  exports:[PokemonModule]
 })
 export class PokemonModule {}
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,31 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+
+
+@Injectable()
+export class SeedService {
+  
+  constructor(
+    private readonly httpService: HttpService,
+  ){}
+
+  async executeSeed(){
+
+    const { data } = await firstValueFrom(
+      this.httpService.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1')
+    )
+
+    data.results.forEach( ({name, url}) => {
+      const segments = url.split('/')
+      const no: number = +segments[ segments.length - 2]
+      console.log({name, no})
+    })
+    
+    return data.results;
+  }
+
+ 
+}
